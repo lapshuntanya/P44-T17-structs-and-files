fbc397f5408e8ae4395154809fd45112b6a91be6
lapshuntanya <lapshun.tatiana@gmail.com>
2025-06-02 17:44:44 +0300

    save

--- P44-T17-structs-and-files.cpp
+++ P44-T17-structs-and-files.cpp
@@ -1,14 +1,28 @@
 #include "Card.h"
+#define CREATE_OR_LOAD 1 //1-create, 0-load
 
 int main()
 {
     srand(time(0));
+    FILE* file = nullptr;
 
     Card pupkin;
-    pupkin.fillCard();
+#if CREATE_OR_LOAD == 1
+    pupkin.fillCard(); //create card
+#else
+    //load card
+#endif
 
     cout << endl;
     pupkin.printTitle();
     pupkin.printLine();
+    pupkin.cash += 50;
+
+    fopen_s(&file, "bank.txt", "w");
+    // file =  fopen("bank.txt", "w");
+    if (file != nullptr) {
+        pupkin.saveToTextFile(file);
+        fclose(file);
+    }
 }
 
